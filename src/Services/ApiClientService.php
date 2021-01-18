6750b819141b0ce3b087b0d4a6a8e41dc29f5b50
Steve Harmeyer <harmeyersteve@gmail.com>
2021-01-18 16:56:34 +0000

    admin user routes

--- src/Services/ApiClientService.php
+++ src/Services/ApiClientService.php
@@ -97,9 +97,7 @@ class ApiClientService
      */
     public function login(string $email, string $password)
     {
-        $apiToken = $this->request('POST', '/api/v1/user/login', ['email' => $email, 'password' => $password]);
-
-        return json_decode($apiToken);
+        return $this->request('POST', '/api/v1/user/login', ['email' => $email, 'password' => $password], false);
     }
 
     /**
@@ -109,9 +107,7 @@ class ApiClientService
      */
     public function me() : stdClass
     {
-        $me = $this->request('GET', '/api/v1/user', [], true);
-
-        return json_decode($me);
+        return $this->request('GET', '/api/v1/user', [], true);
     }
 
     /**
@@ -123,13 +119,11 @@ class ApiClientService
      */
     public function register(string $name, string $email, string $password) : stdClass
     {
-        $user = $this->request('POST', '/api/v1/user/register', [
+        return $this->request('POST', '/api/v1/user/register', [
             'name' => $name,
             'email' => $email,
             'password' => $password,
-        ], true);
-
-        return json_decode($user);
+        ], false);
     }
 
     /**
@@ -139,9 +133,7 @@ class ApiClientService
      */
     public function update(array $attributes = []) : stdClass
     {
-        $user = $this->request('POST', '/api/v1/user/update', $attributes, true);
-
-        return json_decode($user);
+        return $this->request('POST', '/api/v1/user/update', $attributes, true);
     }
 
     /**
@@ -150,9 +142,7 @@ class ApiClientService
      */
     public function enable2fa() : stdClass
     {
-        $twoFactor = $this->request('GET', '/api/v1/user/enable2fa', [], true);
-
-        return json_decode($twoFactor);
+        return $this->request('GET', '/api/v1/user/enable2fa', [], true);
     }
 
     /**
@@ -174,9 +164,7 @@ class ApiClientService
      */
     public function listAccounts() : stdClass
     {
-        $accounts = $this->request('GET', '/api/v1/bank/account', [], true);
-
-        return json_decode($accounts);
+        return $this->request('GET', '/api/v1/bank/account', [], true);
     }
 
     /**
@@ -186,9 +174,7 @@ class ApiClientService
      */
     public function getAccount(string $uuid) : stdClass
     {
-        $account = $this->request('GET', '/api/v1/bank/account/'.$uuid, [], true);
-
-        return json_decode($account);
+        return $this->request('GET', '/api/v1/bank/account/'.$uuid, [], true);
     }
 
     /**
@@ -197,9 +183,7 @@ class ApiClientService
      */
     public function listTransactions() : stdClass
     {
-        $transactions = $this->request('GET', '/api/v1/bank/transaction', [], true);
-
-        return json_decode($transactions);
+        return $this->request('GET', '/api/v1/bank/transaction', [], true);
     }
 
     /**
@@ -209,14 +193,179 @@ class ApiClientService
      */
     public function getTransaction(string $uuid) : stdClass
     {
-        $transaction = $this->request('GET', '/api/v1/bank/transaction/'.$uuid, [], true);
-
-        return json_decode($transaction);
+        return $this->request('GET', '/api/v1/bank/transaction/'.$uuid, [], true);
     }
 
     /**
      * END USER PAYBETR ENDPOINTS.
      */
+
+    /**
+     * List currencies.
+     * @return \stdClass
+     */
+    public function listCurrencies() : stdClass
+    {
+        return $this->request('GET', '/api/v1/paybetr/currency', [], true);
+    }
+
+    /**
+     * Get currency.
+     * @param string $symbol
+     * @return \stdClass
+     */
+    public function getCurrency(string $symbol) : stdClass
+    {
+        return $this->request('GET', '/api/v1/paybetr/currency/'.$symbol, [], true);
+    }
+
+    /**
+     * Convert currency.
+     * @param string $from
+     * @param string $to
+     * @param float $amount
+     * @return stdClass
+     */
+    public function convertCurrency(string $from, string $to, float $amount = 1) : stdClass
+    {
+        return $this->request('GET', '/api/v1/paybetr/currency/'.$from.'/convert/'.$to.'/'.$amount, [], true);
+    }
+
+    /**
+     * List addresses.
+     * @return \stdClass
+     */
+    public function listAddresses() : stdClass
+    {
+        return $this->request('GET', '/api/v1/paybetr/address', [], true);
+    }
+
+    /**
+     * Get address.
+     * @param string $address
+     * @return \stdClass
+     */
+    public function getAddress(string $address) : stdClass
+    {
+        return $this->request('GET', '/api/v1/paybetr/address/'.$address, [], true);
+    }
+
+    /**
+     * Create address.
+     * @param string $symbol
+     * @return \stdClass
+     */
+    public function createAddress(string $symbol) : stdClass
+    {
+        return $this->request('POST', '/api/v1/paybetr/address', ['symbol' => $symbol], true);
+    }
+
+    /**
+     * List withdrawals.
+     * @return \stdClass
+     */
+    public function listWithdrawals() : stdClass
+    {
+        return $this->request('GET', '/api/v1/paybetr/withdrawal', [], true);
+    }
+
+    /**
+     * Get withdrawal.
+     * @param string $uuid
+     * @return \stdClass
+     */
+    public function getWithdrawal(string $uuid) : stdClass
+    {
+        return $this->request('GET', '/api/v1/paybetr/withdrawal/'.$uuid, [], true);
+    }
+
+    /**
+     * Create withdrawal.
+     * @param string $symbol
+     * @param float $amount
+     * @param $externalId
+     * @return \stdClass
+     */
+    public function createWithdrawal(string $symbol, float $amount, $externalId = null) : stdClass
+    {
+        return $this->request('POST', '/api/v1/paybetr/withdrawal', ['symbol' => $symbol, 'amount' => $amount, 'external_id' => $externalId], true);
+    }
+
+    /**
+     * ADMIN USER ROUTES.
+     */
+
+    /**
+     * List users.
+     * @return \stdClass
+     */
+    public function listUsers() : stdClass
+    {
+        return $this->request('GET', '/api/v1/admin/user');
+    }
+
+    /**
+     * Create user.
+     * @param string $name
+     * @param string $email
+     * @param string $password
+     * @return \stdClass
+     */
+    public function createUser(string $name, string $email, string $password) : stdClass
+    {
+        return $this->request('POST', '/api/v1/admin/user', ['name' => $name, 'email' => $email, 'password' => $password]);
+    }
+
+    /**
+     * Get user.
+     * @param string $uuid
+     * @return \stdClass
+     */
+    public function getUser(string $uuid) : stdClass
+    {
+        return $this->request('GET', '/api/v1/admin/user/'.$uuid);
+    }
+
+    /**
+     * Update user.
+     * @param string $uuid
+     * @param array $params
+     * @return \stdClass
+     */
+    public function updateUser(string $uuid, array $params = []) : stdClass
+    {
+        return $this->request('PUT', '/api/v1/admin/user/'.$uuid, $params);
+    }
+
+    /**
+     * Enable 2fa.
+     * @param string $uuid
+     * @return \stdClass
+     */
+    public function adminEnable2fa(string $uuid) : stdClass
+    {
+        return $this->request('GET', '/api/v1/admin/user/'.$uuid.'/enable2fa');
+    }
+
+    /**
+     * Disable 2fa.
+     * @param string $uuid
+     * @return void
+     */
+    public function adminDisable2fa(string $uuid)
+    {
+        $this->request('GET', '/api/v1/admin/user/'.$uuid.'/disable2fa');
+    }
+
+    /**
+     * Create api token.
+     * @param string $uuid
+     * @return \stdClass
+     */
+    public function createApiToken(string $uuid) : stdClass
+    {
+        return $this->request('GET', '/api/v1/admin/user/'.$uuid.'/apitoken');
+    }
 
     /**
      * Make an api request.
@@ -225,7 +374,7 @@ class ApiClientService
      * @param array $data
      * @return /stdClass
      */
-    public function request(string $method, string $uri, array $data = [], bool $needsAuth = false)
+    public function request(string $method, string $uri, array $data = [], bool $needsAuth = true) : stdClass
     {
         $ch = curl_init($this->getBaseUri().$uri);
         if (in_array($method, ['POST', 'PUT', 'PATCH'])) {
@@ -241,6 +390,6 @@ class ApiClientService
         $response = curl_exec($ch);
         curl_close($ch);
 
-        return $response;
+        return json_decode($response);
     }
 }

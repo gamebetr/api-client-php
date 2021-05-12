2827caafb4f3ecf94294a9516bc421c1d2d5491b
Steve Harmeyer <harmeyersteve@gmail.com>
2021-05-12 17:14:07 +0000

    added domain and host types

---
+++ src/Types/Domain.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Gamebetr\ApiClient\Types;
+
+use Gamebetr\ApiClient\Abstracts\BaseType;
+
+class Domain extends BaseType
+{
+    /**
+     * Type.
+     * @var string
+     */
+    public $type = 'domain';
+
+    /**
+     * Methods.
+     * @var array
+     */
+    protected $methods = [
+        'list' => [
+            'endpoint' => 'globalauth/domains',
+            'method' => 'GET',
+            'requires_authentication' => true,
+        ],
+    ];
+}
---
+++ src/Types/Host.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Gamebetr\ApiClient\Types;
+
+use Gamebetr\ApiClient\Abstracts\BaseType;
+
+class Host extends BaseType
+{
+    /**
+     * Type.
+     * @var string
+     */
+    public $type = 'host';
+}
